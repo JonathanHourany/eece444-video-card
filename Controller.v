0893d887195e3d9ff89695c6be086f547dc18d65
Jon Hourany <jon.hourany@ieee.org>
2013-04-05 15:18:38 -0700

    Further adjusted timings. Timings now use Porches. Still no color on monitor

--- Controller.v
+++ Controller.v
@@ -1,6 +1,6 @@
 /***********************************************************
  * @author		Jon Hourany
- * @date		3/27/13
+ * @date		4/04/13
  * @class		EECE 444
  *
  * @file		Controller.v
@@ -16,30 +16,35 @@
     reg [10:0] h_counter;   /*< Tracks amount of pulses from CLK    				>*/
     reg [19:0] v_counter;   /*< Tracks amount of pulses from H_SYNC 				>*/
 
-    `define H_SYNC_PULSE		11'd96
-	`define H_BACK_PORCH_END	11'd144
-    `define H_FRONT_PORCH_STRT  11'd784
-    `define H_COUNT_MAX     	11'd799
+    `define H_SYNC_PULSE		11'd96		/*< Length of Sync Pulse 			>*/
+	`define H_BACK_PORCH_END	11'd144		/*< Pulse len + Porch Len			>*/
+    `define H_FRONT_PORCH_STRT  11'd784		/*< Front Porch Len - Max			>*/
+    `define H_COUNT_MAX     	11'd799		/*< Max line pulses for resolution 	>*/
 
     `define V_SYNC_PULSE		19'd1600
 	`define V_BACK_PORCH_END	19'd28000
     `define V_FRONT_PORCH_STRT  19'd408800
     `define V_COUNT_MAX     	19'd416799
 	
+	/*** State Machine for H_SYNC ***/
 	always @(*) begin
+		/* If the vertical sync line is not in the display zone, keep H_Sync low */
 		if(!(v_counter > `V_BACK_PORCH_END && v_counter < `V_FRONT_PORCH_STRT)) begin
 			H_SYNC = 0;
 			RGB_EN = 0;
 		end
+		/* If the vertical sync line is in display zone, allow H_Sync to go through its procedure */
 		else begin
 			if (h_counter < `H_SYNC_PULSE) begin
 				H_SYNC = 0;
 				RGB_EN = 0;
 			end
+			/* If H_Sync is in the display zone, enable RGB */
 			else if (h_counter > `H_BACK_PORCH_END && h_counter < `H_FRONT_PORCH_STRT) begin
 				H_SYNC = 1;
 				RGB_EN = 1;
 			end
+			/* During the Front Porch period, disable RGB */
 			else begin
 				H_SYNC = 1;
 				RGB_EN = 0;
@@ -47,6 +52,7 @@
 		end
 	end
 	
+	/*** State Machine for V_SYNC ***/
 	always @(*) begin
 		if (v_counter < `V_SYNC_PULSE) begin
 			V_SYNC = 0;
@@ -56,6 +62,7 @@
 		end
 	end
 	
+	/*** Counter logic ***/
 	always @(posedge CLK) begin
 		if (h_counter >= `H_COUNT_MAX || !NRST) begin
 			h_counter <= 11'b00;
--- Top.v
+++ Top.v
@@ -1,9 +1,9 @@
 /***********************************************************
  * @author		Jon Hourany
- * @date		3/09/13
+ * @date		4/04/13
  * @class		EECE 444
  *
- * @file		ctrl_lines.v
+ * @file		Top.v
  * @proj		Video Card
  ************************************************************/
 module Top(RESET, H_SYNC, V_SYNC, RED);
@@ -13,24 +13,8 @@ module Top(RESET, H_SYNC, V_SYNC, RED);
 	output wire RED;
 	
 	wire rgb_en;
-	
-	//output wire osc_clk;
-	//output reg  BLUE;	//output reg  GREEN;
-	
-	//event reset_trigger; 
-     //event  reset_done_trigger; 
-       
-     //initial begin 
-       //forever begin 
-         //@ (reset_trigger); 
-         //@ (negedge clk); 
-         //reset = 0; 
-         //@ (negedge clk); 
-         //reset = 1; 
-         //-> reset_done_trigger; 
-       //end 
-     //end
-	 
+
+	/*** Test Bench Code ***/
 	 //reg osc_clk, reset;
 	 //initial begin
 		 //#0 reset 	= 0;
@@ -40,7 +24,7 @@ module Top(RESET, H_SYNC, V_SYNC, RED);
 	 
 	 //always #1 osc_clk = ~osc_clk;
 	 
-	OSCC OSCC_1 (.OSC(osc_clk));
+	OSCC OSCC_1 (.OSC(osc_clk));	/*< IP clock module for Lattice CPLD	>*/
 	Controller CNTRL(.NRST(RESET), .CLK(osc_clk), .H_SYNC(H_SYNC), .V_SYNC(V_SYNC), .RGB_EN(rgb_en));
 	
 	assign RED = (rgb_en ? 1:1'bz); 
